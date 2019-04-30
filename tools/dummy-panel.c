9095d0b79c7f4782190f157bcb5ce91d7420f796
Zander Brown <zbrown@gnome.org>
2019-05-01 00:02:12 +0100

    debug tool to run panel in an existing session

---
+++ tools/dummy-panel.c
@@ -0,0 +1,42 @@
+#include <gtk/gtk.h>
+
+#include "panel.h"
+
+int
+main (int argc, char** argv)
+{
+  GtkWidget *panel;
+  GtkCssProvider *provider;
+  GFile *file;
+  GError *error = NULL;
+
+  gtk_init (&argc, &argv);
+
+  provider = gtk_css_provider_new ();
+
+  file = g_file_new_for_uri ("resource:///org/raspberry-pi/maynard/style.css");
+
+  if (!gtk_css_provider_load_from_file (provider, file, &error)) {
+    g_warning ("Failed to load CSS file: %s", error->message);
+    g_clear_error (&error);
+    g_object_unref (file);
+  }
+
+  gtk_style_context_add_provider_for_screen (gdk_screen_get_default (),
+      GTK_STYLE_PROVIDER (provider), 600);
+
+  g_object_unref (file);
+
+  g_object_set (gtk_settings_get_default (),
+                "gtk-application-prefer-dark-theme", TRUE,
+                NULL);
+
+  panel = maynard_panel_new ();
+  gtk_window_set_decorated (GTK_WINDOW (panel), TRUE);
+  gtk_window_set_default_size (GTK_WINDOW (panel), 700, 32);
+  gtk_widget_show_all (panel);
+
+  gtk_main ();
+
+  return 0;
+}
